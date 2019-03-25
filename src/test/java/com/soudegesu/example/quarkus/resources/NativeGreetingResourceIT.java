15dcb353a6de48210a6c57f903088b6a5f2cde4b
soudegesu <soudegesu@hotmail.co.jp>
2019-03-25 10:39:39 +0900

    add test codes

---
+++ src/test/java/com/soudegesu/example/quarkus/resources/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.soudegesu.example.quarkus.resources;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
