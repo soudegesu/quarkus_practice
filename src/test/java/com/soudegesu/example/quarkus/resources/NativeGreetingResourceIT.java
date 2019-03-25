28eceb8648ac5f468b7db3e0de6a11527317abdd
soudegesu <soudegesu@hotmail.co.jp>
2019-03-26 08:37:03 +0900

    add spring dependency

--- src/test/java/com/soudegesu/example/quarkus/resources/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.soudegesu.example.quarkus.resources;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
